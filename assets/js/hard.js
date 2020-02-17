8cc22d0712f4c31743b9daa06688fd333827f8ce
Raphael Satomi <raphaelsatomiandrade@gmail.com>
2020-02-17 01:53:32 -0300

    Add time

--- assets/js/hard.js
+++ assets/js/hard.js
@@ -13,18 +13,23 @@ function bojack(){
     
     document.body.appendChild(bojack_adult);    
     l++;
-    if(l == 5){                
+    if(l == 5){
+        c = 1;
         stop();
         if(newx > 1024){
             document.getElementById("all_lost").style.display = "block";
             document.getElementById("image").style.display = "block";
             document.getElementById("image").style.backgroundImage = "url('images/you_lose.png')";
+            clearTimeout(t);
+            clearTimeout(t2);
         }else{
             document.getElementById("all_lost").style.display = "block";
             document.getElementById("mobile_finish").style.display = "block";
             document.getElementById("mobile_finish2").style.display = "block";
             document.getElementById("image").style.display = "block";
             document.getElementById("image").style.backgroundImage = "url('images/you_lose_mobile.png')";
+            clearTimeout(t);
+            clearTimeout(t2);
         }
     }
     console.log("Aumentou: "+l);
@@ -45,16 +50,18 @@ function time(){
 function stop(){    
     clearTimeout(t);
     clearTimeout(t2);
-    if(newx > 1024){
-        document.getElementById("all_lost").style.display = "block";
-        document.getElementById("image").style.display = "block";
-        document.getElementById("image").style.backgroundImage = "url('images/you_win.png')";
-    }else{
-        document.getElementById("all_lost").style.display = "block";
-        document.getElementById("mobile_finish").style.display = "block";
-        document.getElementById("mobile_finish2").style.display = "block";
-        document.getElementById("image").style.display = "block";
-        document.getElementById("image").style.backgroundImage = "url('images/you_win_mobile.png')";
+    if(c == 0){
+        if(newx > 1024){
+            document.getElementById("all_lost").style.display = "block";
+            document.getElementById("image").style.display = "block";
+            document.getElementById("image").style.backgroundImage = "url('images/you_win.png')";
+        }else{
+            document.getElementById("all_lost").style.display = "block";
+            document.getElementById("mobile_finish").style.display = "block";
+            document.getElementById("mobile_finish2").style.display = "block";
+            document.getElementById("image").style.display = "block";
+            document.getElementById("image").style.backgroundImage = "url('images/you_win_mobile.png')";
+        }
     }
 }
 var l = 0;
@@ -65,22 +72,84 @@ var seconds = 30;
 var catch1 = 0;
 var newx = 0;
 var newy = 0;
+var c = 0;
+var n = 0;
+
+
+var n3 = 0;
+var n2 = 0;
+var n1 = 0;
+var n0 = 0;
+var n_1 = 0;
+var n_z = 0;
+
+var m = 0;
+
 function stop_start(){
     clearTimeout(o);
 }
+function stop_n3(){
+    clearTimeout(n3);
+}
+function stop_n2(){
+    clearTimeout(n2);
+}
+function stop_m(){
+    clearTimeout(n1);
+}
+function stop_n1(){
+    clearTimeout(n1);    
+    n_z = setInterval(stop_n111, 0);
+    m = setInterval(stop_n_1,1);
+    clearTimeout(m);
+}
+function stop_count(){
+    clearTimeout(n0);
+}
+function stop_n_1(){
+    clearTimeout(n_z);
+}
+function stop_n111(){
+    document.getElementById("all_lost").style.display = "none";
+    document.getElementById("image").style.display = "none";        
+    clearTimeout(m);
+}
+function num1(){    
+    document.getElementById("image").style.backgroundImage = "url('images/1.png')";    
+    stop_n111();
+    o = setInterval(start2, 0);
+    setInterval(stop_start, 0);
+}
+function num_m(){    
+    document.getElementById("image").style.backgroundImage = "url('images/1.png')";    
+    m = setInterval(num1, 1000);
+    stop_m();
+}
+function num2(){    
+    document.getElementById("image").style.backgroundImage = "url('images/2.png')";    
+    n1 = setInterval(num_m, 1000);
+    stop_n2();
+}
+function num3(){
+    document.getElementById("all_lost").style.display = "block";
+    document.getElementById("image").style.display = "block";
+    document.getElementById("image").style.backgroundImage = "url('images/3.png')";    
+    n2 = setInterval(num2, 1000);   
+    stop_n3(); 
+}
+
 function start(){
-    o = setInterval(start2, 1000);
-    setInterval(stop_start, 1000);
+    n3 = setInterval(num3, 0);
 }
 
 function start2(){        
-    t = setInterval(bojack, 500);      
+    t = setInterval(bojack, 250);      
     t2 = setInterval(time, 1000);
     setInterval(stop, 30000);          
 }
 
-function restart(x){    
-    if(x.keyCode == 32){
+function restart(x){        
+    if(x.keyCode == 109){
         window.location.href = "hard.html";
     }
     if(x.keyCode == 113){
